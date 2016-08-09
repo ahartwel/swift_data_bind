dd6c6a33e420d174be7de26bac1b08133b3071c1
ahartwel <hartwellalex@gmail.com>
2016-08-08 21:42:27 -0400

    Initial Commit

---
+++ DataBind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataBind
+//
+//  Created by Alex Hartwell on 8/8/16.
+//  Copyright © 2016 Alex Hartwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
